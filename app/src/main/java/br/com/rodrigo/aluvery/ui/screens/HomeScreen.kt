97b5b084f54297a36d6df2557a03c95289d784d4
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2022-12-24 18:18:13 -0300

    create search field

--- app/src/main/java/br/com/rodrigo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/rodrigo/aluvery/ui/screens/HomeScreen.kt
@@ -1,11 +1,19 @@
 package br.com.rodrigo.aluvery.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,18 +24,35 @@ import br.com.rodrigo.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(sections: Map<String, List<Product>>) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        item { Spacer(Modifier) }
 
-        sections.map { (title, products) ->
-            item { ProductSection(title, products) }
+    Column(modifier = Modifier) {
+        var txtSearch by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = txtSearch,
+            label = { Text("Product") },
+            shape = RoundedCornerShape(100),
+            onValueChange = { value -> txtSearch = value },
+            placeholder = { Text("What are you looking for?") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                .fillMaxWidth()
+        )
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            item { Spacer(Modifier) }
+
+            sections.map { (title, products) ->
+                item { ProductSection(title, products) }
+            }
+
+            item { Spacer(Modifier) }
         }
-
-        item { Spacer(Modifier) }
     }
 }
 
