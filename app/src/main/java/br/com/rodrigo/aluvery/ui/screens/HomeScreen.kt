7087559449d86bcb38d4a72ea1baf710b0573dea
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2022-12-25 13:24:49 -0300

    Add feature search product

--- app/src/main/java/br/com/rodrigo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/rodrigo/aluvery/ui/screens/HomeScreen.kt
@@ -2,24 +2,18 @@ package br.com.rodrigo.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.rodrigo.aluvery.models.Product
+import br.com.rodrigo.aluvery.sampledata.sampleProducts
 import br.com.rodrigo.aluvery.sampledata.sampleSections
+import br.com.rodrigo.aluvery.ui.components.CardProductItem
 import br.com.rodrigo.aluvery.ui.components.ProductSection
+import br.com.rodrigo.aluvery.ui.components.SearchTextField
 import br.com.rodrigo.aluvery.ui.theme.AluveryTheme
 
 @Composable
@@ -28,30 +22,37 @@ fun HomeScreen(sections: Map<String, List<Product>>) {
     Column(modifier = Modifier) {
         var txtSearch by remember { mutableStateOf("") }
 
-        OutlinedTextField(
-            value = txtSearch,
-            label = { Text("Product") },
-            shape = RoundedCornerShape(100),
-            onValueChange = { value -> txtSearch = value },
-            placeholder = { Text("What are you looking for?") },
-            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
-                .fillMaxWidth()
-        )
+        val searchedProducts = remember(txtSearch) {
+            if(txtSearch.isNotBlank()) {
+                sampleProducts.filter { p ->
+                    p.name.contains(txtSearch, ignoreCase = true) ||
+                            p.description?.contains(txtSearch, ignoreCase = true) ?: false
+                }
+            } else {
+                emptyList()
+            }
+        }
+
+        SearchTextField(txtSearch, onSearchChange = { search -> txtSearch = search })
 
         LazyColumn(
+            contentPadding = PaddingValues(bottom = 16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            item { Spacer(Modifier) }
-
-            sections.map { (title, products) ->
-                item { ProductSection(title, products) }
+            if (!txtSearch.isBlank()) {
+                items(searchedProducts) { product ->
+                    CardProductItem(
+                        product = product,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            } else {
+                sections.map { (title, products) ->
+                    item { ProductSection(title, products) }
+                }
             }
-
-            item { Spacer(Modifier) }
         }
     }
 }
